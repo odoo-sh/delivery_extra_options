71adb69b9200e7978d74f847fc4c5e2d8094b897
Sivaprakash <sivaprakash@sodexis.com>
2022-09-15 18:39:01 +0530

    [ID:25433]Migrate delivery_ups_custom_packages to V15 - Corrections

--- delivery_ups_custom_packages/models/__init__.py
+++ delivery_ups_custom_packages/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021 Sodexis
+# Copyright 2021-2022 Sodexis
 # License OPL-1 (See LICENSE file for full copyright and licensing details).
 
 from . import ups_request
-from . import product_packaging
+from . import stock_package_type
